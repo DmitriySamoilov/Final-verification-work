90a9145be1177e1abfd7d437ff075b35df77cbe0
Dmitriy Samoilov <ddd2008ok@gmail.com>
2023-02-27 12:02:22 +0300

    Program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//string [] arrayFirst={"hello","2", "world", ":-)"};
+string [] arrayFirst={"1234","1567", "-2", "computer science"};
+//string [] arrayFirst={"Russia","Denmark", "Kazan"};
+System.Console.WriteLine($"[{string.Join(", ", arrayFirst)}]");
+string [] arraySecond= FindStringBelow4(arrayFirst);
+System.Console.WriteLine($"[{string.Join(", ", arraySecond)}]");
+
+string [] FindStringBelow4(string[] arrayStart)
+{
+int n=0,j=0;
+foreach (string item in arrayStart)
+{
+   if (item.Length<4) n++;
+}
+string []arrayResult= new string [n];
+foreach (string item in arrayStart)
+{
+   if (item.Length<4) {arrayResult[j]=item; j++;}
+}
+return arrayResult;
+}
